83581c3900e11a77f03cc574aacfd1d4feded6b9
Gipnokotik <36129002+Gipnokot@users.noreply.github.com>
2024-10-03 06:51:02 +0500

    Create Old_drink.php

---
+++ Codewars/Old_drink.php
@@ -0,0 +1,19 @@
+function people_with_age_drink(int $old): string 
+{
+  if ($old < 14) 
+  {
+    return "drink toddy";
+  }
+  elseif ($old >= 14 && $old < 18) 
+  {
+    return "drink coke";
+  }
+  elseif ($old >= 18 && $old < 21) 
+  {
+    return "drink beer";
+  }
+  else 
+  {
+    return "drink whisky";
+  }
+}
